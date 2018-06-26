aef108143346b87701757b2110c42277f6d8773b
konojunya <junya03dance@gmail.com>
2018-06-26 11:29:07 +0900

    fizzbuzz

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,4 +1,13 @@
 defmodule Fizzbuzz do
+  def main(args) do
+    x = String.to_integer hd args
+    cond do
+      rem(x, 3) == 0 and rem(x, 5) == 0 -> IO.puts "FizzBuzz"
+      rem(x, 3) == 0 -> IO.puts "Fizz"
+      rem(x, 5) == 0 -> IO.puts "Buzz"
+      true -> IO.puts x
+    end
+  end
   @moduledoc """
   Documentation for Fizzbuzz.
   """
